0b6f0a1eaa4bbdc386256dc4ec8a8b01a06d5ae0
Hrvoje Krhac <hrvoje.krhac@gmail.com>
2023-06-02 12:48:11 +0200

    build error

--- src/components/homepage/CountUp.tsx
+++ src/components/homepage/CountUp.tsx
@@ -1,19 +1,9 @@
-import { useState, useRef } from 'react'
+import React from 'react'
 
-const CountUp = ({ end, time }) => {
-	const [Value, setValue] = useState(null);
-	const ref = useRef(0)
-
-	const acc = end / 200
-	const updateCouterState = () => (
-		if (ref.current < end) {
-		const res = Math.ceil(ref.current + acc)
-	}
-		)
+const CountUp = () => {
+	return (
+		<div>CountUp</div>
 	)
-return (
-	<div>CountUp</div>
-)
 }
 
 export default CountUp
